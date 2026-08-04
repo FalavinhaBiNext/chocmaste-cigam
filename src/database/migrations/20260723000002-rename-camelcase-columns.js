b8d4bc34e4821c6834186a8d9a1a3e13fb056ae9
Guilherme Oliveira <guilherme.oliveira@falavinha.com.br>
2026-08-04 15:58:23 -0300

    Ajuste na migration

--- src/database/migrations/20260723000002-rename-camelcase-columns.js
+++ src/database/migrations/20260723000002-rename-camelcase-columns.js
@@ -3,16 +3,36 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.renameColumn('produtos', 'descricaoCurta', 'descricao_curta');
-    await queryInterface.renameColumn('produtos', 'tipoProduto', 'tipo_produto');
-    await queryInterface.renameColumn('produtos', 'fornecedor_precoCusto', 'fornecedor_preco_custo');
-    await queryInterface.renameColumn('produtos', 'temVariacoes', 'tem_variacoes');
+    const table = await queryInterface.describeTable('produtos');
+
+    const renames = [
+      ['descricaoCurta', 'descricao_curta'],
+      ['tipoProduto', 'tipo_produto'],
+      ['fornecedor_precoCusto', 'fornecedor_preco_custo'],
+      ['temVariacoes', 'tem_variacoes'],
+    ];
+
+    for (const [oldName, newName] of renames) {
+      if (table[oldName] && !table[newName]) {
+        await queryInterface.renameColumn('produtos', oldName, newName);
+      }
+    }
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.renameColumn('produtos', 'descricao_curta', 'descricaoCurta');
-    await queryInterface.renameColumn('produtos', 'tipo_produto', 'tipoProduto');
-    await queryInterface.renameColumn('produtos', 'fornecedor_preco_custo', 'fornecedor_precoCusto');
-    await queryInterface.renameColumn('produtos', 'tem_variacoes', 'temVariacoes');
+    const table = await queryInterface.describeTable('produtos');
+
+    const renames = [
+      ['descricao_curta', 'descricaoCurta'],
+      ['tipo_produto', 'tipoProduto'],
+      ['fornecedor_preco_custo', 'fornecedor_precoCusto'],
+      ['tem_variacoes', 'temVariacoes'],
+    ];
+
+    for (const [oldName, newName] of renames) {
+      if (table[oldName] && !table[newName]) {
+        await queryInterface.renameColumn('produtos', oldName, newName);
+      }
+    }
   }
 };
